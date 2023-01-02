2464b66e3f5c7186ca81949aaf9adf3b7ba51c6e
Tak, Jaein | Jane | LPD <jaein.tak@rakuten.com>
2023-01-02 15:24:57 +0900

    run locally

--- src/setup.js
+++ src/setup.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React from "react";
+import "./setup.css";
 
-export default function Setup({handleSelection}) {
- 
+export default function Setup({ handleSelection }) {
   return (
-    <div>
+    <div className="container">
+      <div className="title">Abducted</div>
       <p>How many players?</p>
-      <button onClick={() => handleSelection(1)}>1</button>
-      <button onClick={() => handleSelection(2)}>2</button>
-      <button onClick={() => handleSelection(3)}>3</button>
-      <button onClick={() => handleSelection(4)}>4</button>
+      <button className="player-btn" onClick={() => handleSelection(1)}>
+        1
+      </button>
+      <button className="player-btn" onClick={() => handleSelection(2)}>
+        2
+      </button>
+      <button className="player-btn" onClick={() => handleSelection(3)}>
+        3
+      </button>
+      <button className="player-btn" onClick={() => handleSelection(4)}>
+        4
+      </button>
     </div>
   );
 }
